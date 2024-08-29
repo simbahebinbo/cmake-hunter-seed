ce27863b5828a0bd09111a049e993f560e140e90
simbahebinbo <lansheng228@163.com>
2024-08-29 17:48:13 +0800

    Update

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -3,7 +3,11 @@ hunter_config(
         VERSION 1.86.0
         URL "https://boostorg.jfrog.io/artifactory/main/release/1.86.0/source/boost_1_86_0.tar.bz2"
         SHA1 fd0d26a7d5eadf454896942124544120e3b7a38f
-        CMAKE_ARGS CMAKE_POSITION_INDEPENDENT_CODE=ON
+        CMAKE_ARGS
+        CMAKE_POSITION_INDEPENDENT_CODE=ON
+        Boost_USE_STATIC_LIBS=ON   # 使用静态库
+        Boost_USE_MULTITHREADED=ON
+        Boost_USE_STATIC_RUNTIME=OFF
 )
 
 hunter_config(
--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -3,8 +3,7 @@
 
 # https://docs.hunter.sh/en/latest/packages/pkg/GTest.html
 hunter_add_package(GTest)
-find_package(GTest CONFIG REQUIRED)
-find_package(GMock CONFIG REQUIRED)
+find_package(GTest CONFIG REQUIRED COMPONENTS GTest GMock)
 
 # https://docs.hunter.sh/en/latest/packages/pkg/Boost.html
 hunter_add_package(Boost)
